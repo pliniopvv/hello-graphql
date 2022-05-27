9ce06b5c5c07452dd0709ea233b8bbaa8a7d5026
pliniopvw <gangss2@hotmail.com>
2022-05-27 11:57:12 -0300

    Curso COMPLETO de GraphQL #3: Introdução | INSTALAR AS FERRAMENTAS

---
+++ index.js
@@ -0,0 +1,26 @@
+const { gql, ApolloServer} = require('apollo-server');
+
+const resolvers = {
+    Query: {
+        hello() {
+            return 'World';
+        }
+    }
+};
+
+
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+
+
+server.listen();
